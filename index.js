2451a581ab80e872cd9d36b90af25dda6b31c580
coderkhushal <137866759+coderkhushal@users.noreply.github.com>
2023-10-08 11:40:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const port = process.env.PORT || 5000
 const cors= require("cors")
 require('dotenv').config();
 
-app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json())
 const connecttomongoose=require("./db")
 connecttomongoose()
